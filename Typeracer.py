130a7bc8afd79a5eb367a2c268d0b177bbe86390
Nikola Saratlija <nikola@brandsync.nl>
2019-07-04 14:03:33 +0200

    added a help command. variable name changes.

--- Typeracer.py
+++ Typeracer.py
@@ -9,7 +9,7 @@ from asyncio import sleep
 load_dotenv()
 TOKEN = getenv("DISCORD_BOT_TOKEN")
 
-texts = [
+TEXTS = [
     "You are what you are and you are where you are because of what has gone into your mind. You change what you are and you change where you are by changing what goes into your mind.",
     "What you don't have you don't need it now. What you don't know you can feel it somehow. What you don't have you don't need it now.",
     "When I need to find something out, I just go out and look for somebody that knows more than me, and I go and ask them. Sometimes I ask pretty hard.",
@@ -19,24 +19,31 @@ texts = [
 
 
 class Typeracer(discord.Client):
+    __JOINING_PHASE_TIMER: int = 10
+
     participants: List[discord.Member] = []
-    race_text: str = ""
+    current_text: str = ""
     is_joining_phase: bool = False
     race_is_started: bool = False
-    JOINING_PHASE_TIMER: int = 10
 
     async def on_message(self, message):
         # ignores the messages by the bot itself
         if message.author == self.user:
             return
 
-        # can start a race by typing a certain command
-        if message.content == '/typeracer start':
-            if self.is_joining_phase is True:
-                return await message.channel.send("A race has already been started.")
+        # prints a list of commands
+        if message.content == '/typeracer help':
+            await message.channel.send(
+                "Command list: \n"
+                "`/typeracer help`: A list of commands. \n"
+                "`/typeracer start`: Starts a new race. Client will be given 10 seconds to join the race. \n"
+                "`/typeracer join`: Adds the client who typed this command to a race if one has been started."
+            )
 
+        # can start a race by typing a certain command
+        if message.content == '/typeracer start' and self.is_joining_phase is False:
             await self.announce_race(message.channel)
-            await sleep(self.JOINING_PHASE_TIMER)
+            await sleep(self.__JOINING_PHASE_TIMER)  # this waiting period gives clients time to join the race
             await self.countdown(message.channel)
             await self.send_random_text(message.channel)
 
@@ -45,23 +52,22 @@ class Typeracer(discord.Client):
             await message.channel.send("<@" + str(message.author.id) + ">" + " has joined the race!")
             self.participants.append(message.author)
 
+        # members who type out the text correctly, win
         if self.race_is_started is True:
-            # members who type out the text correctly, win
-            if message.author in self.participants and message.content == self.race_text:
+            if message.author in self.participants and message.content == self.current_text:
                 # messages the winners
                 await message.channel.send("<@" + str(message.author.id) + ">" + " got it right!")
-                self.race_text = ""
+                self.current_text = ""
 
     async def announce_race(self, channel: discord.TextChannel):
         self.is_joining_phase = True
         await channel.send(
             "A new race has started! "
-            "You have " + str(self.JOINING_PHASE_TIMER) + " seconds to type `/typeracer join` to participate.")
+            "You have " + str(self.__JOINING_PHASE_TIMER) + " seconds to type `/typeracer join` to participate.")
 
     async def send_random_text(self, channel: discord.TextChannel):
-        # picks a random text and messages it to the server
-        self.race_text = texts[randint(0, len(texts) - 1)]
-        await channel.send(self.race_text)
+        self.current_text = TEXTS[randint(0, len(TEXTS) - 1)]
+        await channel.send(self.current_text)
 
     async def countdown(self, channel: discord.TextChannel):
         self.is_joining_phase = False
@@ -70,7 +76,7 @@ class Typeracer(discord.Client):
         await channel.send("Ready.")
         await sleep(1)
         await channel.send("Set.")
-        await sleep(randint(1, 3))
+        await sleep(randint(1, 3))  # This interval is random because I thought it would be funny.
         await channel.send("Type!")
         self.race_is_started = True
 
