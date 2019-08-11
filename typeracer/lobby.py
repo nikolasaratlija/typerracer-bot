153582fbdca0e37975d0cb906f6636c7c91c064b
Nikola Saratlija <saratlijanikola@gmail.com>
2019-08-11 14:03:05 +0300

    added `host` attribute to Lobby

--- typeracer/lobby.py
+++ typeracer/lobby.py
@@ -1,16 +1,23 @@
-from discord import TextChannel
+from discord import TextChannel, Member
 from typing import List
 from .player import Player
 
 
 class Lobby:
     players: List[Player] = []
-    is_started: bool = False
+    race_is_started: bool = False
     current_text: str = ""
+    host: Member
 
-    def __init__(self, lobby_id, channel: TextChannel):
+    def __init__(self, lobby_id, channel: TextChannel, host: Member):
         self.lobby_id = lobby_id
         self.channel = channel
+        self.set_host(host)
 
     def add_player(self, player: Player):
         self.players.append(player)
+        self.channel.set_permissions(player, read_messages=True, send_messages=True)
+
+    def set_host(self, host: Member):
+        self.host = host
+        self.channel.set_permissions(self.host, read_messages=True, send_messages=True)
