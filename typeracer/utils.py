232bf725e2b39426e5fa34b44840f5139a349b94
Nikola Saratlija <nikola@brandsync.nl>
2019-09-21 23:48:07 +0200

    added function which can find a specific lobby in a list by providing an id

--- typeracer/utils.py
+++ typeracer/utils.py
@@ -7,4 +7,4 @@ class Utils:
     def find_lobby_by_id(channel_id: int, channel_list: List[TextChannel]):
         return next(
             channel for channel in channel_list
-            if next(int(s) for s in channel.name.split() if s.isdigit()) == channel_id)
+            if next(int(string) for string in channel.name.split() if string.isdigit()) == channel_id)
