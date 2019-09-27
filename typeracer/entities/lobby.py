17d02572710a5199d1114e6233202ee94162c434
Nikola Saratlija <nikola@brandsync.nl>
2019-09-28 00:42:33 +0200

    added utility functions

--- typeracer/entities/lobby.py
+++ typeracer/entities/lobby.py
@@ -1,5 +1,7 @@
 from discord import TextChannel, Member
 from typing import List
+
+from typeracer.exceptions import LobbyNotFound
 from typeracer.entities.player import Player
 
 
@@ -17,6 +19,7 @@ class Lobby:
         player = Player(member)
         self.players.append(player)
         await self.channel.set_permissions(player.member, read_messages=True, send_messages=True)
+        await self.channel.send(f"{member.mention} has joined lobby the lobby!")
 
     async def set_host(self, host: Member):
         self.host = host
@@ -24,7 +27,20 @@ class Lobby:
         await self.channel.send(f"{self.host.mention} is the host of this lobby.")
 
     @staticmethod
-    def find_lobby_by_id(channel_id: int, channel_list: List[TextChannel]):
-        return next(
-            channel for channel in channel_list
-            if next(int(string) for string in channel.name.split() if string.isdigit()) == channel_id)
+    def get_lobby_by_channel(channel: TextChannel, lobby_list: List['Lobby']):
+        try:
+            return next(lobby for lobby in lobby_list if lobby.channel == channel)
+        except StopIteration:
+            return StopIteration
+
+    @staticmethod
+    def get_lobby_by_id(lobby_id: int, lobby_list: List['Lobby']):
+        try:
+            return next(lobby for lobby in lobby_list if lobby.lobby_id == lobby_id)
+        except StopIteration:
+            raise LobbyNotFound
+
+    @staticmethod
+    def get_id_from_string(string: str):
+        """extracts the first number from a string and returns it as a string"""
+        return next(int(word) for word in string.split() if word.isdigit())
