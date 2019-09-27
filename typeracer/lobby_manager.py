42fd5e08fa466ba163f2baf96b8f5d1fc88c4662
Nikola Saratlija <nikola@brandsync.nl>
2019-09-28 00:44:37 +0200

    utilizing new check and utility functions, improved importing

--- typeracer/lobby_manager.py
+++ typeracer/lobby_manager.py
@@ -1,12 +1,12 @@
+from discord.ext import commands
+
 from typing import List
 from random import randint
 from json import load
 
-from typeracer.entities.lobby import Lobby
-from .checks import *
-
-from discord.ext import commands
-from discord import Member
+from .entities.lobby import Lobby
+import typeracer.exceptions as exceptions
+import typeracer.checks as checks
 
 
 class LobbyManager(commands.Cog):
@@ -20,11 +20,6 @@ class LobbyManager(commands.Cog):
     def manage(self, lobby: Lobby):
         self.lobbies.append(lobby)
 
-    @staticmethod
-    async def add_player(lobby: Lobby, member: Member):
-        await lobby.add_player(member)
-        await lobby.channel.send(f"{member.mention} has joined lobby the lobby!")
-
     def get_random_text(self):
         return self.TEXTS[randint(0, len(self.TEXTS) - 1)]
 
@@ -34,38 +29,38 @@ def setup(bot):
     lobby_manager = bot.get_cog('LobbyManager')
 
     @commands.command()
-    @is_called_from_lobby(lobby_manager.lobbies)
-    @is_lobby_host(lobby_manager.lobbies)
+    @checks.is_called_from_lobby(lobby_manager.lobbies)
+    @checks.is_lobby_host(lobby_manager.lobbies)
     async def start(ctx):
         await ctx.send("Host has started the race!")
         await ctx.send(lobby_manager.get_random_text())
 
     @start.error
     async def start_error(ctx, error):
-        if isinstance(error, NotCalledFromALobby):
+        if isinstance(error, exceptions.NotCalledFromALobby):
             await ctx.send(f"{ctx.message.author.mention}, this channel is not a Typeracer lobby.")
 
-        if isinstance(error, NotAHost):
+        if isinstance(error, exceptions.NotAHost):
             await ctx.send(f"{ctx.message.author.mention}, you are not the host of this lobby.")
 
     @commands.command()
-    @is_not_called_from_lobby(lobby_manager.lobbies)
-    @user_not_in_lobby(lobby_manager.lobbies)
-    # @lobby_exists(LobbyManager.lobbies)
+    @checks.lobby_exists(lobby_manager.lobbies)
+    @checks.is_not_called_from_lobby(lobby_manager.lobbies)
+    @checks.user_not_in_lobby(lobby_manager.lobbies)
     async def join(ctx, lobby_id):
-        try:
-            lobby = next(lobby for lobby in lobby_manager.lobbies if lobby.lobby_id == lobby_id)
-            await lobby_manager.add_player(lobby, ctx.message.author)
-        except StopIteration:
-            raise LobbyNotFound
+        lobby = Lobby.get_lobby_by_id(lobby_id, lobby_manager.lobbies)
+        await lobby.add_player(ctx.message.author)
 
     @join.error
     async def join_error(ctx, error):
-        if isinstance(error, DuplicatePlayer):
+        if isinstance(error, exceptions.DuplicatePlayer):
             await ctx.send(f"{ctx.message.author.mention}, you're already in a lobby.")
 
-        if isinstance(error, LobbyNotFound):
+        if isinstance(error, exceptions.LobbyNotFound):
             await ctx.send(f"{ctx.message.author.mention}, the channel you're trying to join does not exist.")
+
+        if isinstance(error, exceptions.CalledFromALobby):
+            await ctx.send(f"{ctx.message.author.mention}, you can't use this command in this channel.")
 
     bot.add_command(start)
     bot.add_command(join)
